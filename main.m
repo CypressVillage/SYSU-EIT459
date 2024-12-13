e8504ed2597b50a8ca5fc4f060d3886de2152b3b
Cypress <2169626027@qq.com>
2024-12-13 14:51:42 +0800

    🚧 取消保存吞吐量、误码率

--- main.m
+++ main.m
@@ -213,13 +213,13 @@ toc(startTime);
 % save('TransmitSignal.mat', 'var4_1')
 
 %% 保存吞吐量、误码率到.txt文件
-% 保存吞吐量
-throughput = mean(downlinkResults.userResults.throughput.values);
-% 保存误码率
-BER = mean(downlinkResults.userResults.BERUncoded.values);
-fprintf(2, 'Throughput: %f\n', throughput);
-fprintf(2, 'BER: %f\n', BER);
-return
+% % 保存吞吐量
+% throughput = mean(downlinkResults.userResults.throughput.values);
+% % 保存误码率
+% BER = mean(downlinkResults.userResults.BERUncoded.values);
+% fprintf(2, 'Throughput: %f\n', throughput);
+% fprintf(2, 'BER: %f\n', BER);
+% return
 
 %% 绘图
 close all;
